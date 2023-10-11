eaa5040fdd8ede6b5bfe9dc204a7b21b049015ca
Eliana Torres <elianatorres3114@gmail.com>
2023-10-11 19:41:58 -0400

    fixes

---
+++ script.js
@@ -0,0 +1,44 @@
+let numRows = 0;
+let numCols = 0;
+let colorSelected; 
+
+// Add a row
+function addR() {
+    let newRow = document.getElementById("");
+}
+
+// Add a column
+function addC() {
+    alert("Clicked Add Col"); // Replace this line with your code.
+}
+
+// Remove a row
+function removeR() {
+    alert("Clicked Remove Row"); // Replace this line with your code.
+}
+
+// Remove a column
+function removeC() {
+    alert("Clicked Remove Col"); // Replace this line with your code.
+}
+
+// Set global variable for selected color
+function selectColor(){
+    colorSelected = document.getElementById("selectedColorId").value;
+    console.log(colorSelected);
+}
+
+// Fill all uncolored cells
+function fillU(){
+    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+}
+
+// Fill all cells
+function fillAll(){
+    alert("Clicked Fill All"); // Replace this line with your code.
+}
+
+// Clear all cells
+function clearAll(){
+    alert("Clicked Clear All"); // Replace this line with your code.
+}
